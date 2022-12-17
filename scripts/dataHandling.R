ffe017bb0ca88653167bdace91ee0dee14272d3d
nreinken <niklas.reinken1@uol.de>
2022-12-17 17:11:07 +0100

    implement simple function for loading data from csv and select columns to keep
    #9

--- scripts/dataHandling.R
+++ scripts/dataHandling.R
@@ -3,13 +3,45 @@
 #based on scripts by Niklas Reinken, July 2021
 #version 1, December 2022
 
-if(!require(cli)){install.packages("cli")}
-if(!require(readr)){install.packages("readr")}
-library(cli)
-library(readr)
+if(!require(tidyverse)){install.packages("tidyverse")}
+if(!require(janitor)){install.packages("janitor")}
+library(tidyverse)
 
-data.loadData <- function()
+
+#whichColumns = c("letter_rec", "junc_border", "graph_complexity")
+data.loadData <- function(whichColumns = "", removeWaZ = T, removeWordEnds = F)
 {
-  data <- readr::read_csv2("../Graphen_MAIN.csv")
-  return(data)
+  d <- readr::read_csv2("Graphen_MAIN.csv")
+  d <- janitor::remove_empty(d, which = c("rows", "cols"))
+  
+  #convert to factors and integers
+  d <- d %>% mutate_if(is.character,as.factor)
+  d <- d %>% mutate_if(is.double,as.integer)
+  
+  
+  #remove line break separations
+  if(removeWaZ)
+  {
+    d <- filter(d, d$WaZ != T)
+  }
+  d$WaZ <- NULL
+  
+  
+  #remove word ends
+  if(removeWordEnds)
+  {
+    d <- filter(d, d$word_struc != "fin")
+  }
+  d$word_struc <- NULL
+  
+  #select desired columns
+  if(length(whichColumns) > 0)
+  {
+    d <- dplyr::select(d, any_of(whichColumns))
+  }
+ 
+  d <- droplevels(d)
+  
+  print(d)
+  return(d)
 }
