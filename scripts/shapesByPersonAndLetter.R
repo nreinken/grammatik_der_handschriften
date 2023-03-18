41dc1c1fbb16fed363bac67986e332079ecae796
nreinken <niklas.reinken1@uol.de>
2023-03-18 17:15:02 +0100

    reworking analysis of letter shape numbers for each person
    #11
    #2

--- scripts/shapesByPersonAndLetter.R
+++ scripts/shapesByPersonAndLetter.R
@@ -1,46 +1,49 @@
 #shapesByPersonAndLetter.R
 #calculates the amount of letter shapes each individual text contains for each letter
-#© Niklas Reinken, July 2021
+#based on scripts by Niklas Reinken, July 2021
+#version 2, March 2023
+
 
 #load libraries
-library(janitor)
+if(!require(tidyverse)){install.packages("tidyverse")}
+if(!require(plyr)){install.packages("plyr")}
 library(tidyverse)
 
+source("scripts/dataHandling.R")
+
+options(scipen = 999)
 
 #load data
-d <- read_csv2("../Graphen_MAIN.csv")
-d <- remove_empty(d)
+d <- data.loadData(whichColumns = c("person_ID","letter_rec","code"), removeWaZ = F, removeWordEnds = F, removeUpperCase = T, removeUnrecognisable = T)
 
-#only use necessary columns
-d <- select(d, person_ID, letter, code_neu)
-
-#recode letter names for Umlaute
-d$letter <- plyr::revalue(d$letter, c("ä" = "a", "ö" = "o", "ü" = "u"))
 
 #group data for each letter shape
-d_group_extract <- group_by(d, code_neu)
+shapePersons <- group_by(d, code)
 #count the amount of persons each letter is used by
-personProForm <- summarize(d_group_extract, n = n_distinct(person_ID))
+shapePersons <- dplyr::summarize(shapePersons, nPersons = n_distinct(person_ID))
 #save to .csv
-write_csv2(personProForm, "PersonProForm.csv")
-
-#omit unrecognizable letter shapes
-d <- filter(d, !code_neu %in% c("0", "a99", "b99", "c99", "d99", "e99", "f99", "g99", "h99", "i99", "j99", "k99", "l99", "m99", "n99", "o99", "p99", "q99", "r99", "s99", "t99", "u99", "v99", "w99", "x99", "y99", "z99", "ß99"))
+write_csv2(shapePersons, "results/numberPersonsEachShape.csv")
+#clean up 
+rm(shapePersons)
 
 #group by person and letter
-d_group <- group_by(d, person_ID, letter)
+d <- group_by(d, person_ID, letter_rec)
 #count the amount of letter shapes for each person
-formenProPerson <- summarize(d_group, n = n_distinct(code_neu))
+d <- dplyr::summarize(d, nShapes = n_distinct(code))
 #write to .csv
-write_csv2(formenProPerson, "formenProPerson.csv")
+write_csv2(d, "results/numberShapesEachPerson.csv")
 
 #group by person
-formenProPersonGroup <- group_by(formenProPerson, person_ID)
+persons <- group_by(d, person_ID)
 #calculate median amount of letter shapes for each person
-formenProPersonGroupMedian <- summarize(formenProPersonGroup, x = mean(n))
-median(formenProPersonGroupMedian$x)
+persons <- dplyr::summarize(persons, x = mean(nShapes))
+median(persons$x)
 
 #group by letter
-formenProPersonLetter <- group_by(formenProPerson, letter)
+letters <- group_by(d, letter_rec)
 #calculate median amount of letter shapes for each letter
-formenProPersonLetterMedian <- summarize(formenProPersonLetter, x = mean(n))
+letters <- dplyr::summarize(letters, x = mean(nShapes))
+median(letters$x)
+
+#clean up
+rm(d, letters, persons)
